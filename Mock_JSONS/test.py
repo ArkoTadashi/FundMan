941b9f2c8995dd591df3943d1494daaf043ba942
ArkoTadashi <aurosikder@gmail.com>
2024-01-27 17:19:30 +0600

    ppp

--- Mock_JSONS/test.py
+++ Mock_JSONS/test.py
@@ -2,10 +2,10 @@ import random
 
 file = open("test.txt", 'w')
 
-p = int(12)
+p = int(24)
 
 for i in range(p):
-    file.write(str(random.randint(26, 60)/100))
+    file.write(str(random.randint(44000, 46000)))
     if i != p-1:
         file.write(", ")
 file.close()
