8020a291b7161b481505375dd1672a13732ed2dd
azerom-azerom <asarnob9063@gmail.com>
2024-02-18 22:21:04 +0600

    daily

--- Backend/index.js
+++ Backend/index.js
@@ -346,6 +346,20 @@ app.get("/market/:token/yearly",(req,res)=>{
     })
 })
 
+//daily
+app.get("/market/:token/daily",(req,res)=>{
+
+    database.collection('market')
+    .findOne({token: req.params.token}, {projection: {dailyPrice: 1, _id: 0}}) //cursor
+    .then((entry)=>{
+        console.log(entry)
+        res.status(200).json(entry)
+    })  
+    .catch(()=>{
+        res.status(500).json({err:'Market collection fetching err'})
+    })
+})
+
 //signup
 app.post("/signup",async (req, res) => {
     const entry = req.body;
